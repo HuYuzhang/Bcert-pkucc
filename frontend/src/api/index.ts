bc33d73f67803a04e68f4bd804897151a7bf973c
ddadaal <ddadaal@outlook.com>
2020-10-14 19:30:45 +0800

    download cert is working

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,7 +1,6 @@
 const API_ROOT = "http://127.0.0.1:3000";
+const ipfsRoot = "https://ipfs.io/ipfs";
 
 export const uploadCertPath = API_ROOT + "/upload";
 
-export const ipfsRoot = "https://ipfs.io" + "/ipfs";
-
-export const downloadCertPath = (hash: string) => ipfsRoot + "/" + hash;
+export const ipfsDownloadPath = (hash: string) => ipfsRoot + "/" + hash;
