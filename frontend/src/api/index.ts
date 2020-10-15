4f33f948004758968ff01b3494630b83884ded88
ddadaal <ddadaal@outlook.com>
2020-10-15 11:14:45 +0800

    use env for api urls

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,5 +1,5 @@
-const API_ROOT = "http://127.0.0.1:3000";
-const ipfsRoot = "https://ipfs.io/ipfs";
+const API_ROOT = process.env.API_ROOT || "http://127.0.0.1:3000";
+const ipfsRoot = process.env.IPFS_ROOT || "https://ipfs.io/ipfs";
 
 export const uploadCertPath = API_ROOT + "/upload";
 
