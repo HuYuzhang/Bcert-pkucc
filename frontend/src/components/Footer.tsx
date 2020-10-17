a900442d4652817610552132dcf9b08b350d3276
Chen Junda <ddadaal@outlook.com>
2020-10-17 17:10:35 +0800

    improve footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,13 +1,21 @@
 import React from "react";
+import { Col, Container, Row } from "reactstrap";
+import "./Footer.css";
 
 export const Footer: React.FC = () => {
   return (
-    <div className="bottom">
-	    <ul className="down">
-        <li>© All Rights Reserved </li>
-        <li>基于区块链的教育信息验证系统 </li>
-        <li>时旻 毕业设计</li>
-	    </ul>
-    </div>
+    <Container className="footer" fluid>
+      <Row>
+        <Col md={4} className="footer-item">
+          © All Rights Reserved
+        </Col>
+        <Col md={4} className="footer-item">
+          基于区块链的教育信息验证系统
+        </Col>
+        <Col md={4} className="footer-item">
+          时旻 毕业设计
+        </Col>
+      </Row>
+    </Container>
   );
 };
